548c4f3fa30be25d20a7ad19d4df4cb59f777628
badabumb <juliavediukova@mail.ru>
2024-11-27 18:21:40 +0300

    Второй коммит

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const http = require('http');
-const hostname = '0.0.0.0'; 
-const port = 8080; 
+
+// Используем порт из переменной окружения или 8080 по умолчанию
+const port = process.env.PORT || 8080;
+const hostname = '0.0.0.0';
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Docker with Node.js!\n');
 });
+
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
